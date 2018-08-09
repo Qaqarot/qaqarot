72d7493a959e6382f758de1a1a62ea4870b42ade
gyu-don <takumi.kt+git@gmail.com>
2018-08-09 20:38:15 +0900

    modify qaoa

--- examples_qaoa.py
+++ examples_qaoa.py
@@ -32,15 +32,16 @@ class MaxcutQaoa:
         print(p)
         maxi = p.argmax()
         maxi_bitstring = ("{:0" + str(n_qubits) + "b}").format(maxi)[::-1]
+        self.result = list(maxi_bitstring)
         print("Most significant index:", maxi_bitstring)
 
     def get_circuit(self, gammas, betas):
         c = Circuit(self.n_qubits)
         c.h[:]
         for gamma, beta in zip(gammas, betas):
-            c.rx(beta)[:]
             for i,j in self.edges:
                 c.cx[i, j].rz(gamma)[j].cx[i, j]
+            c.rx(beta)[:]
         return c
 
     def get_objective_func(self):
@@ -65,4 +66,10 @@ class MaxcutQaoa:
         return obj_f
 
 if __name__ == "__main__":
-    MaxcutQaoa(2, 200, [(0, 1), (1, 2), (2, 3), (3, 0), (1, 3), (0, 2), (4, 0), (4, 3)])
+    ma = MaxcutQaoa(2, 250, [(0, 1), (1, 2), (2, 3), (3, 0), (1, 3), (0, 2), (4, 0), (4, 3)])
+    print("""
+       {4}
+      / \\
+     {0}---{3}
+     | x |
+     {1}---{2}""".format(*ma.result))
