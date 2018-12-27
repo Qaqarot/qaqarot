8cd26ca3a04f371930f749960c6764adfc812ce1
gyu-don <takumi.kt+git@gmail.com>
2018-12-27 16:46:18 +0900

    Debug to_inttuple

--- blueqat/utils.py
+++ blueqat/utils.py
@@ -5,7 +5,7 @@ def to_inttuple(bitstr):
     if isinstance(bitstr, str):
         return tuple(int(b) for b in bitstr)
     if isinstance(bitstr, Counter):
-        return Counter((tuple(int(b) for b in k), v) for k, v in bitstr.items())
+        return Counter({tuple(int(b) for b in k): v for k, v in bitstr.items()})
     if isinstance(bitstr, dict):
         return {tuple(int(b) for b in k): v for k, v in bitstr.items()}
     raise ValueError("bitstr type shall be `str`, `Counter` or `dict`")
