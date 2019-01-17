4be635d9bb96e0ffaee879580b6a215964a96452
mdr_minato <minato@mdrft.com>
2019-01-17 21:01:01 +0900

    add module opt

--- blueqat/__init__.py
+++ blueqat/__init__.py
@@ -2,5 +2,6 @@ from .circuit import Circuit, BlueqatGlobalSetting
 from . import pauli
 from . import utils
 from . import vqe
+from . import opt
 
-__all__ = ["pauli", "utils", "vqe", "Circuit", "BlueqatGlobalSetting"]
+__all__ = ["pauli", "utils", "vqe", "opt", "Circuit", "BlueqatGlobalSetting"]
