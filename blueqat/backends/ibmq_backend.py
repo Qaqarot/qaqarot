f5cdb64e78331f4287245638ff40b369c0b86d11
gyu-don <takumi.kt+git@gmail.com>
2019-09-24 19:26:40 +0900

    Improve IBMQ backend

--- blueqat/backends/ibmq_backend.py
+++ blueqat/backends/ibmq_backend.py
@@ -18,11 +18,15 @@ To run this backend, call
 Circuit.run_with_ibmq or Circuit.run(backend="ibmq", ...).
 
 Args:
-    qiskit_backend: Backend of qiskit.
+    qiskit_backend (Backend, optional): Backend of qiskit.
+        If this argument is omitted, Blueqat uses Qiskit Aer's qasm_simulator.
     shots (int, optional): The number of shots.
-    returns (str, optional): You can specify "shots", "qiskit_result" or "_exception".
+    returns (str, optional): Choose from followings. Default is "shots".
         "shots": Default behavior. Returns the measurement result.
-        "qiskit_result": Returns result which is returned by Qiskit.
+        "draw": Draw the circuit via `qiskit.circuit.QuantumCircuit.draw()`.
+        "qiskit_circuit": Returns Qiskit's circuit class. (qiskit.circuit.QuantumCircuit)
+        "qiskit_job": Returns Qiskit's job class. (qiskit.providers.BaseJob)
+        "qiskit_result": Returns Qiskit's result class, which is returns by BaseJob.result().
         "_exception": This is for troubleshooting purpose.
                       Returns the exception which raised during importing Qiskit.
                       If no exception is raised, returns None.
@@ -36,17 +40,19 @@ from collections import Counter
 from .qasm_parser_backend_generator import generate_backend
 
 
-def _qasm_runner_qiskit(qasm, qiskit_backend, shots=None, returns=None, **kwargs):
+def _qasm_runner_qiskit(qasm, qiskit_backend=None, shots=None, returns=None, **kwargs):
     if returns is None:
         returns = "shots"
-    elif returns not in ("shots", "_exception", "qiskit_result"):
-        raise ValueError("`returns` shall be None, 'shots', 'qiskit_result' or '_exception'")
+    elif returns not in ("shots", "draw", "_exception",
+                         "qiskit_circuit", "qiskit_job", "qiskit_result"):
+        raise ValueError("`returns` shall be None, 'shots', 'draw', " +
+                         "'qiskit_circuit', 'qiskit_job', 'qiskit_result' or '_exception'")
 
     import_error = None
     try:
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            from qiskit import QuantumCircuit, execute
+            from qiskit import Aer, QuantumCircuit, execute
     except Exception as e:
         import_error = e
 
@@ -60,10 +66,21 @@ def _qasm_runner_qiskit(qasm, qiskit_backend, shots=None, returns=None, **kwargs
             raise ValueError("Unknown error raised when importing qiskit. To get exception, " +
                              'run this backend with arg `returns="_exception"`')
     else:
+        if returns == "_exception":
+            return None
+        qk_circuit = QuantumCircuit.from_qasm_str(qasm)
+        if returns == "qiskit_circuit":
+            return qk_circuit
+        if returns == "draw":
+            return qk_circuit.draw(**kwargs)
         if shots is None:
             shots = 1024
-        qk_circuit = QuantumCircuit.from_qasm_str(qasm)
-        result = execute(qk_circuit, backend=qiskit_backend, shots=shots, **kwargs).result()
+        if qiskit_backend is None:
+            qiskit_backend = Aer.get_backend("qasm_simulator")
+        job = execute(qk_circuit, backend=qiskit_backend, shots=shots, **kwargs)
+        if returns == "qiskit_job":
+            return job
+        result = job.result()
         if returns == "qiskit_result":
             return result
         counts = Counter({bits[::-1]: val for bits, val in result.get_counts().items()})
