0c5bd6753058d0a1b499752f83b600fa3755d8ed
gyu-don <takumi.kt+git@gmail.com>
2020-10-20 22:41:33 +0900

    ENH: Add one qubit transpiler

--- blueqat/backends/__init__.py
+++ blueqat/backends/__init__.py
@@ -4,6 +4,7 @@ from .qasm_output_backend import QasmOutputBackend
 from .ibmq_backend import ibmq_backend
 from .sympy_backend import SympyBackend
 from .qgate_backend import QgateBackend
+from .onequbitgate_transpiler import OneQubitGateCompactionTranspiler
 from .twoqubitgate_transpiler import TwoQubitGateDecomposingTranspiler
 
 BACKENDS = {
@@ -13,6 +14,7 @@ BACKENDS = {
     "ibmq": ibmq_backend,
     "sympy_unitary": SympyBackend,
     "qgate": QgateBackend,
-    "twoqubitgate_decompose": TwoQubitGateDecomposingTranspiler,
+    "2q_decomposition": TwoQubitGateDecomposingTranspiler,
+    "1q_compaction": OneQubitGateCompactionTranspiler,
 }
 DEFAULT_BACKEND_NAME = "numpy"
