92378d304ca5af694c8224af1b9b9de163b3ab3f
gyu-don <takumi.kt+git@gmail.com>
2021-09-02 22:11:09 +0900

    TST: Add tests for mcx_gray

--- tests/test_macros.py
+++ tests/test_macros.py
@@ -42,3 +42,48 @@ def test_mcz_gray_4():
             circuit_to_unitary(Circuit().mcz_gray([0, 1, 2, 3], 4)),
             np.diag([1] * 31 + [-1])
     )
+
+def test_c3x():
+    expected = np.eye(16)
+    expected[7, 7] = expected[15, 15] = 0
+    expected[7, 15] = expected[15, 7] = 1
+    np.allclose(
+            circuit_to_unitary(Circuit().c3x(0, 1, 2, 3)),
+            expected
+    )
+
+def test_c4x():
+    expected = np.eye(32)
+    expected[15, 15] = expected[31, 31] = 0
+    expected[15, 31] = expected[31, 15] = 1
+    np.allclose(
+            circuit_to_unitary(Circuit().c4x(0, 1, 2, 3, 4)),
+            expected
+    )
+
+def test_mcx_gray_0():
+    np.allclose(
+            circuit_to_unitary(Circuit().mcx_gray([], 0)),
+            np.array([[0, 1], [1, 0]])
+    )
+
+def test_mcz_gray_1():
+    np.allclose(
+            circuit_to_unitary(Circuit().mcz_gray([0], 1)),
+            circuit_to_unitary(Circuit().cx[0, 1])
+    )
+
+def test_mcz_gray_2():
+    np.allclose(
+            circuit_to_unitary(Circuit().mcz_gray([0, 1], 2)),
+            circuit_to_unitary(Circuit().ccx[0, 1, 2])
+    )
+
+def test_mcx_gray_4():
+    expected = np.eye(32)
+    expected[15, 15] = expected[31, 31] = 0
+    expected[15, 31] = expected[31, 15] = 1
+    np.allclose(
+            circuit_to_unitary(Circuit().mcx_gray([0, 1, 2, 3], 4)),
+            expected
+    )
