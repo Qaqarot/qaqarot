924cbb118454b27bfe17781a8f0a11b01baf449a
gyu-don <takumi.kt+git@gmail.com>
2021-09-03 19:43:25 +0900

    TST: Add tests for mcrx, mcry, mcrz

--- tests/test_macros.py
+++ tests/test_macros.py
@@ -1,3 +1,4 @@
+from math import pi
 import math
 import cmath
 import random
@@ -12,78 +13,109 @@ from blueqat.circuit_funcs import circuit_to_unitary
 import blueqat.macros
 
 PAULI_X = np.array([[0, 1], [1, 0]])
+ANGLES = [0.0, pi / 4, pi / 2, pi, -pi / 2, 2 * pi, -7 * pi]
+
 
 def test_c3z():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().c3z(0, 1, 2, 3)),
-            np.diag([1] * 15 + [-1])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().c3z(0, 1, 2, 3)),
+                       np.diag([1] * 15 + [-1]))
+
 
 def test_c4z():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().c4z(0, 1, 2, 3, 4)),
-            np.diag([1] * 31 + [-1])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().c4z(0, 1, 2, 3, 4)),
+                       np.diag([1] * 31 + [-1]))
+
 
 def test_mcz_gray_0():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().mcz_gray([], 0)),
-            np.diag([1, -1])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().mcz_gray([], 0)),
+                       np.diag([1, -1]))
+
 
 def test_mcz_gray_1():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().mcz_gray([0], 1)),
-            np.diag([1, 1, 1, -1])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().mcz_gray([0], 1)),
+                       np.diag([1, 1, 1, -1]))
+
 
 def test_mcz_gray_4():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().mcz_gray([0, 1, 2, 3], 4)),
-            np.diag([1] * 31 + [-1])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().mcz_gray([0, 1, 2, 3], 4)),
+                       np.diag([1] * 31 + [-1]))
+
 
 def test_c3x():
     expected = np.eye(16)
     expected[7, 7] = expected[15, 15] = 0
     expected[7, 15] = expected[15, 7] = 1
-    assert np.allclose(
-            circuit_to_unitary(Circuit().c3x(0, 1, 2, 3)),
-            expected
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().c3x(0, 1, 2, 3)), expected)
+
 
 def test_c4x():
     expected = np.eye(32)
     expected[15, 15] = expected[31, 31] = 0
     expected[15, 31] = expected[31, 15] = 1
-    assert np.allclose(
-            circuit_to_unitary(Circuit().c4x(0, 1, 2, 3, 4)),
-            expected
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().c4x(0, 1, 2, 3, 4)),
+                       expected)
+
 
 def test_mcx_gray_0():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().mcx_gray([], 0)),
-            np.array([[0, 1], [1, 0]])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().mcx_gray([], 0)),
+                       np.array([[0, 1], [1, 0]]))
+
 
 def test_mcx_gray_1():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().mcx_gray([0], 1)),
-            circuit_to_unitary(Circuit().cx[0, 1])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().mcx_gray([0], 1)),
+                       circuit_to_unitary(Circuit().cx[0, 1]))
+
 
 def test_mcx_gray_2():
-    assert np.allclose(
-            circuit_to_unitary(Circuit().mcx_gray([0, 1], 2)),
-            circuit_to_unitary(Circuit().ccx[0, 1, 2])
-    )
+    assert np.allclose(circuit_to_unitary(Circuit().mcx_gray([0, 1], 2)),
+                       circuit_to_unitary(Circuit().ccx[0, 1, 2]))
+
 
 def test_mcx_gray_4():
     expected = np.eye(32)
     expected[15, 15] = expected[31, 31] = 0
     expected[15, 31] = expected[31, 15] = 1
+    assert np.allclose(circuit_to_unitary(Circuit().mcx_gray([0, 1, 2, 3], 4)),
+                       expected)
+
+
+@pytest.mark.parametrize("theta", ANGLES)
+@pytest.mark.parametrize("n", [0, 1, 2, 4])
+def test_mcrx_gray_n(theta, n):
+    u = circuit_to_unitary(Circuit().rx(theta)[0])
+    expected = np.eye(2**(n + 1), dtype=complex)
+    expected[2**n - 1, 2**n - 1] = u[0, 0]
+    expected[2**(n + 1) - 1, 2**n - 1] = u[1, 0]
+    expected[2**n - 1, 2**(n + 1) - 1] = u[0, 1]
+    expected[2**(n + 1) - 1, 2**(n + 1) - 1] = u[1, 1]
     assert np.allclose(
-            circuit_to_unitary(Circuit().mcx_gray([0, 1, 2, 3], 4)),
-            expected
-    )
+        circuit_to_unitary(Circuit().mcrx_gray(theta, list(range(n)), n)),
+        expected)
+
+
+@pytest.mark.parametrize("theta", ANGLES)
+@pytest.mark.parametrize("n", [0, 1, 2, 4])
+def test_mcry_gray_n(theta, n):
+    u = circuit_to_unitary(Circuit().ry(theta)[0])
+    expected = np.eye(2**(n + 1), dtype=complex)
+    expected[2**n - 1, 2**n - 1] = u[0, 0]
+    expected[2**(n + 1) - 1, 2**n - 1] = u[1, 0]
+    expected[2**n - 1, 2**(n + 1) - 1] = u[0, 1]
+    expected[2**(n + 1) - 1, 2**(n + 1) - 1] = u[1, 1]
+    assert np.allclose(
+        circuit_to_unitary(Circuit().mcry_gray(theta, list(range(n)), n)),
+        expected)
+
+
+@pytest.mark.parametrize("theta", ANGLES)
+@pytest.mark.parametrize("n", [0, 1, 2, 4])
+def test_mcrz_gray_n(theta, n):
+    u = circuit_to_unitary(Circuit().rz(theta)[0])
+    expected = np.eye(2**(n + 1), dtype=complex)
+    expected[2**n - 1, 2**n - 1] = u[0, 0]
+    expected[2**(n + 1) - 1, 2**n - 1] = u[1, 0]
+    expected[2**n - 1, 2**(n + 1) - 1] = u[0, 1]
+    expected[2**(n + 1) - 1, 2**(n + 1) - 1] = u[1, 1]
+    assert np.allclose(
+        circuit_to_unitary(Circuit().mcrz_gray(theta, list(range(n)), n)),
+        expected)
