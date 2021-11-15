87e8239fd11e5e63a726a0d43c0156a71d246dfe
gyu-don <takumi.kt+git@gmail.com>
2021-11-15 21:55:29 +0900

    API: Change flatten behaviour if measurement has key options

--- blueqat/circuit_funcs/flatten.py
+++ blueqat/circuit_funcs/flatten.py
@@ -6,10 +6,18 @@ def flatten(c: Circuit) -> Circuit:
     n_qubits = c.n_qubits
     ops = []
     for op in c.ops:
-        if isinstance(op, (g.OneQubitGate, g.Measurement, g.Reset)):
+        if isinstance(op, (g.OneQubitGate, g.Reset)):
             ops += [op.create(t, op.params) for t in op.target_iter(n_qubits)]
         elif isinstance(op, g.TwoQubitGate):
             ops += [op.create(t, op.params) for t in op.control_target_iter(n_qubits)]
+        elif isinstance(op, g.Measurement):
+            if op.key is None:
+                ops += [op.create(t, op.params) for t in op.target_iter(n_qubits)]
+            else:
+                options = {'key': op.key}
+                if op.duplicated is not None:
+                    options['duplicated'] = op.duplicated
+                ops += [op.create(tuple(t for t in op.target_iter(n_qubits)), op.params, options)]
         else:
             raise ValueError(f"Cannot process operation {op.lowername}.")
     return Circuit(n_qubits, ops)
